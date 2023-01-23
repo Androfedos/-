65e768aa6811d19e9f4395bda52357fbf4c33c08
Androfedos <99863699+Androfedos@users.noreply.github.com>
2023-01-23 14:28:06 +0300

    Update shifr.py

--- shifr.py
+++ shifr.py
@@ -2,7 +2,7 @@ alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
 
 link = input('Добро пожаловать в шифровщик Цезаря, введите путь к файлу, который вы хотите зашифровать: ')
 parol = int(input('Введите число - ключ шифрования, и обязательно запомните его, если захотите расшифровать текст обратно:')) % 36
-raznica = 33
+raznica = len(alphabet)
 mode = input('введите Y, если хотите зашифровать текст, и N, если расшифровать ')
 if mode == 'N':
     parol = -parol
@@ -18,7 +18,7 @@ for line in f:
     for char in line.lower():
         if char in alphabet:
             simbol = alphabet.index(char)
-            if simbol + parol < 33 and simbol + parol >= 0:
+            if simbol + parol < len(alphabet) and simbol + parol >= 0:
                 copyline += alphabet[simbol + parol]
             else:
                 copyline += alphabet[simbol + parol - raznica]
